5be3d4cc49f96fc1e457c2cdd6831845bb8665b9
Jeremy <jeremya@codingtemple.com>
2025-04-15 09:56:57 -0600

    Starter code from the live lesson.

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+function changeTitle() {
+    let title = document.getElementById("title");
+    title.innerHTML = "This title was changed by JavaScript.";
+}
+
+function greetUser() {
+    alert("Hello, user!");
+}
+
+// greetUser();
