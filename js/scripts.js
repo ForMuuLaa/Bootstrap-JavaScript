e64e640e6384e38195adc5bc2cb5a39284a7d567
ForMuuLaa <ps4formula@gmail.com>
2025-05-16 03:03:26 -0400

    first

--- js/scripts.js
+++ js/scripts.js
@@ -7,4 +7,46 @@ function greetUser() {
     alert("Hello, user!");
 }
 
+
+  function toggleTheme(button) {
+    const body = document.body;
+    const currentTheme = body.getAttribute("data-bs-theme");
+
+    if (currentTheme === "dark") {
+      body.setAttribute("data-bs-theme", "light");
+      button.textContent = "Switch to Dark Mode";
+    } else {
+      body.setAttribute("data-bs-theme", "dark");
+      button.textContent = "Switch to Light Mode";
+    }
+  }
+
+
+function toggle_class_banner(element, class_added) {
+    if (element.classList.contains(class_added)) {
+        element.classList.remove(class_added);
+    } else {
+        element.classList.add(class_added);
+    }
+}
+
+
+function toggle_class_narbar(element, class_added, class_removed) {
+    if (element.classList.contains(class_added)) {
+        element.classList.remove(class_added);
+        element.classList.add(class_removed);
+    } else {
+        element.classList.remove(class_removed);
+        element.classList.add(class_added);
+    }
+}
+
+const button = document.getElementById("button");
+const div = document.getElementById("main-banner");
+const nav = document.getElementById("navbar");
+
+button.addEventListener("click", () => {
+    toggle_class_banner(div, "bg-body-tertiary");
+    toggle_class_narbar(nav, "bg-dark", "bg-body-tertiary");
+});
 // greetUser();
